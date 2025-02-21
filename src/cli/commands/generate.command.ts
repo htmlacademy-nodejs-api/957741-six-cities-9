080a00eeb4c5f20adc6390b9c66f470de08837a8
Yuriy <yura.posledov@yandex.ru>
2025-02-21 14:34:53 +0300

    2.10. Добавит запрос для получения данных. WIP

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,16 +1,35 @@
+import got from 'got';
 import { Command } from './command.interface.js';
 import { CommandName } from '../constants.js';
+import { MockServerData } from '../../shared/types/mock-server-data.type.js';
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
   public getName(): CommandName {
     return CommandName.GENERATE;
   }
 
-  public execute(...parameters: string[]): void {
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
-    // Код для получения данных с сервера.
-    // Формирование объявлений.
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
