92613680ad3a23ebf1a519f9e1e06bbc4f3ecf4f
Yuriy <yura.posledov@yandex.ru>
2025-03-06 22:17:29 +0300

    3.17. Обновит класс RestConfig

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,10 +1,12 @@
 import { config, DotenvParseOutput } from 'dotenv';
 
+import { configRestSchema, RestSchema } from './rest.schema.js';
+
 import { Logger } from '../logger/logger.interface.js';
 import { Config } from './config.interface.js';
 
-export class RestConfig implements Config {
-  private readonly config: NodeJS.ProcessEnv;
+export class RestConfig implements Config<RestSchema> {
+  private readonly config: RestSchema;
 
   constructor(
     private readonly logger: Logger
@@ -15,11 +17,14 @@ export class RestConfig implements Config {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
 
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
     this.logger.info('.env file found and successfully parsed!');
   }
 
-  public get(key: string): string | undefined {
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
     return this.config[key];
   }
 }
