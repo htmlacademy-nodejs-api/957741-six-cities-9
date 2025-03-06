20d76ee648cd1f31bd61a759a199861122d31991
Yuriy <yura.posledov@yandex.ru>
2025-03-06 22:52:06 +0300

    3.25. Добавит вывод сообщения в PinoLogger

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -36,6 +36,7 @@ export class PinoLogger implements Logger {
     });
 
     this.logger = pino({}, multiTransport);
+    this.logger.info('Logger created…');
   }
 
   public debug(message: string, ...args: unknown[]): void {
