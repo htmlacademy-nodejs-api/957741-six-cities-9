37e0a336c2213730e4fb84fcb6ef87cca20641bd
Yuriy <yura.posledov@yandex.ru>
2025-03-16 19:35:26 +0300

    4.9. Добавит подключение к базе данных

--- src/shared/libs/config/rest.schema.type.ts
+++ src/shared/libs/config/rest.schema.type.ts
@@ -2,7 +2,7 @@ export type RestSchema = {
   PORT: number;
   SALT: string;
   DB_HOST: string;
-  DB_PORT: number;
+  DB_PORT: string;
   DB_NAME: string;
   DB_USER: string;
   DB_PASSWORD: string;
