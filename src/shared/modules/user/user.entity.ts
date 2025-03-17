4ced10dc120dfc110249b936febeba8cf1285615
Yuriy <yura.posledov@yandex.ru>
2025-03-17 15:37:04 +0300

    4.17. Добавит определение модели и схемы. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User, UserType } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop()
   public name: string;
+
+  @prop({ unique: true })
   public email: string;
+
+  @prop()
   public avatar: string;
+
+  @prop()
   public password: string;
+
+  @prop()
   public userType: UserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
