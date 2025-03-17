dfaec88fc71a30db534a905307d6acc6d26ffea9
Yuriy <yura.posledov@yandex.ru>
2025-03-17 16:19:09 +0300

    4.20. Добавит методы в UserEntity. WIP

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -5,3 +5,4 @@ export {
   getErrorMessage,
 } from './common.js';
 export { getMongoURI } from './database.js';
+export { createSHA256 } from './hash.js';
--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,27 +1,36 @@
-import { modelOptions, getModelForClass, prop } from '@typegoose/typegoose';
-
+import { getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 import { User, UserType } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
 @modelOptions({
   schemaOptions: {
+    collection: 'users',
     timestamps: true,
   }
 })
 export class UserEntity implements User {
-  @prop()
-  public name: string;
+  @prop({ unique: true, required: true })
+  public email!: string;
 
-  @prop({ unique: true })
-  public email: string;
+  @prop({ default: '' })
+  public avatar!: string;
 
-  @prop()
-  public avatar: string;
+  @prop({ required: true, default: '' })
+  public name!: string;
 
-  @prop()
-  public password: string;
+  @prop({ required: true, default: '' })
+  public password!: string;
 
-  @prop()
-  public userType: UserType;
+  @prop({ required: true, default: '' })
+  public userType!: UserType;
+
+  constructor(userData: User, salt: string) {
+    this.email = userData.email;
+    this.avatar = userData.avatar;
+    this.name = userData.name;
+    this.userType = userData.userType;
+    this.password = createSHA256(this.password, salt);
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
