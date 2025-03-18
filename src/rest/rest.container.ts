44150025fa693b698b0d216f6033444033ca47db
Yuriy <yura.posledov@yandex.ru>
2025-03-18 22:46:29 +0300

    4.24. Разобьёт общий контейнер. WIP

---
+++ src/rest/rest.container.ts
@@ -0,0 +1,16 @@
+import { Container, ContainerModule } from 'inversify';
+
+import { RestApplication } from './rest.application.js';
+import { COMPONENT_MAP } from '../shared/types/index.js';
+import { Logger, PinoLogger } from '../shared/libs/logger/index.js';
+import { Config, RestConfig, RestSchema } from '../shared/libs/config/index.js';
+import { DatabaseClient, MongoDatabaseClient } from '../shared/libs/database-client/index.js';
+
+export function createRestApplicationModule() {
+  return new ContainerModule(({ bind }) => {
+    bind<RestApplication>(COMPONENT_MAP.REST_APPLICATION).to(RestApplication).inSingletonScope();
+    bind<Logger>(COMPONENT_MAP.LOGGER).to(PinoLogger).inSingletonScope();
+    bind<Config<RestSchema>>(COMPONENT_MAP.CONFIG).to(RestConfig).inSingletonScope();
+    bind<DatabaseClient>(COMPONENT_MAP.DATABESE_CLIENT).to(MongoDatabaseClient).inSingletonScope();
+  });
+}
