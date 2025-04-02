ff873c63206e827bcf6643d46ebd4ed81855f2d7
Yuriy <yura.posledov@yandex.ru>
2025-04-02 19:34:04 +0300

    6.17. Добавим обработку HttpError в AppExceptionFilter

--- src/shared/libs/rest/exception-filter/app-exception-filter.ts
+++ src/shared/libs/rest/exception-filter/app-exception-filter.ts
@@ -2,9 +2,12 @@ import { inject, injectable } from 'inversify';
 import { StatusCodes } from 'http-status-codes';
 import { NextFunction, Request, Response } from 'express';
 
+import { createErrorObject } from '../../../helpers/common.js';
+
 import { ExceptionFilter } from './exception-filter.interface.js';
 import { Logger } from '../../logger/index.js';
 import { COMPONENT_MAP } from '../../../types/component-map.enum.js';
+import { HttpError } from '../errors/http-error.js';
 
 @injectable()
 export class AppExceptionFilter implements ExceptionFilter {
@@ -14,10 +17,26 @@ export class AppExceptionFilter implements ExceptionFilter {
     this.logger.info('Register AppExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} — ${error.message}`, error);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
     this.logger.error(error.message, error);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: error.message });
+      .json({ error: error.message })
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    this.handleOtherError(error, req, res, next);
   }
 }
