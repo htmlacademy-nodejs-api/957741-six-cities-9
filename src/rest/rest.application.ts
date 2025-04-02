052c486f30d9f28d9fb75f52b0009d0242bbae24
Yuriy <yura.posledov@yandex.ru>
2025-04-02 17:43:50 +0300

    6.2. Добавит создание и запуск сервера express

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
@@ -8,12 +9,15 @@ import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class RestApplication {
+  private readonly server: Express;
+
   constructor(
     @inject(COMPONENT_MAP.LOGGER) private readonly logger: Logger,
     @inject(COMPONENT_MAP.CONFIG) private readonly config: Config<RestSchema>,
     @inject(COMPONENT_MAP.DATABESE_CLIENT) private readonly databaseClient: DatabaseClient,
   ) {
     this.logger.info('RestApplication created…');
+    this.server = express();
   }
 
   private async initDb() {
@@ -22,21 +26,28 @@ export class RestApplication {
       this.config.get('DB_PASSWORD'),
       this.config.get('DB_HOST'),
       this.config.get('DB_PORT'),
-      this.config.get('DB_NAME'),
+      this.config.get('DB_NAME')
     );
 
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
-    this.logger.info(`Get value from env $SALT: ${this.config.get('SALT')}`);
-    this.logger.info(`Get value from env $DB_HOST: ${this.config.get('DB_HOST')}`);
 
     this.logger.info('Init database…');
     await this.initDb();
-
     this.logger.info('Init database completed');
+
+    this.logger.info('Try to init server…');
+    await this._initServer();
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+    this.logger.info(`Get value from env $SALT: ${this.config.get('SALT')}`);
+    this.logger.info(`Get value from env $DB_HOST: ${this.config.get('DB_HOST')}`);
   }
 }
