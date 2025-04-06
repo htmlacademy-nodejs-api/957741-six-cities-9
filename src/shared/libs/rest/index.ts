a0d6b60eb481f0d12d6e7b9c9c776bd2e081f328
Yuriy <yura.posledov@yandex.ru>
2025-04-06 16:24:55 +0300

    7.14. Добавит middleware UploadFileMiddleware

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -6,6 +6,7 @@ export { Middleware } from './middleware/middleware.interface.js';
 export { ValidateObjectIdMiddleware } from './middleware/validate-objectid.middleware.js';
 export { ValidateDtoMiddleware } from './middleware/validate-dto.middleware.js';
 export { DocumentExistsMiddleware } from './middleware/document-exists.middleware.js';
+export { UploadFileMiddleware } from './middleware/upload-file.middleware.js';
 
 export { Controller } from './controller/controller.interface.js';
 export { BaseController } from './controller/base-controller.abstract.js';
