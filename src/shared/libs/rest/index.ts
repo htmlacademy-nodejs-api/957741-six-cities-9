c8b4b7430387579d5d4f5dad2892453e8a87cc11
Yuriy <yura.posledov@yandex.ru>
2025-04-06 14:33:22 +0300

    7.5. Добавит первую middleware ValidateObjectIdMiddleware

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -3,6 +3,7 @@ export { RequestBody } from './types/request-body.type.js';
 export { HttpMethod } from './types/http-method.enum.js';
 export { Route } from './types/route.interface.js';
 export { Middleware } from './middleware/middleware.interface.js';
+export { ValidateObjectIdMiddleware } from './middleware/validate-objectid.middleware.js';
 
 export { Controller } from './controller/controller.interface.js';
 export { BaseController } from './controller/base-controller.abstract.js';
