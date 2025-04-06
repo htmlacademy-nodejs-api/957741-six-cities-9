2f31203a05603d90b752608f142aace8ba32f63c
Yuriy <yura.posledov@yandex.ru>
2025-04-06 16:33:40 +0300

    7.15. Добавит метод uploadAvatar в UserController

--- src/shared/libs/rest/controller/user.controller.ts
+++ src/shared/libs/rest/controller/user.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import { inject, injectable } from 'inversify';
-import { BaseController, DocumentExistsMiddleware, HttpMethod, ValidateObjectIdMiddleware } from '../index.js';
+import { BaseController, DocumentExistsMiddleware, HttpMethod, UploadFileMiddleware, ValidateObjectIdMiddleware } from '../index.js';
 import { Logger } from '../../logger/index.js';
 import { COMPONENT_MAP } from '../../../types/component-map.enum.js';
 import { UserRdo, UserService } from '../../../modules/user/index.js';
 import { fillDTO } from '../../../helpers/index.js';
 import { OfferService } from '../../../modules/offer/index.js';
+import { Config } from 'convict';
+import { RestSchema } from '../../config/rest.schema.type.js';
 
 @injectable()
 export class UserController extends BaseController {
@@ -13,11 +15,17 @@ export class UserController extends BaseController {
     @inject(COMPONENT_MAP.LOGGER) protected readonly logger: Logger,
     @inject(COMPONENT_MAP.USER_SERVICE) private readonly userService: UserService,
     @inject(COMPONENT_MAP.OFFER_SERVICE) private readonly offerService: OfferService,
+    @inject(COMPONENT_MAP.CONFIG) private readonly config: Config<RestSchema>,
   ) {
     super(logger);
 
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
-    this.addRoute({ path: '/avatar', method: HttpMethod.Put, handler: this.updateAvatar });
+    this.addRoute({
+      path: '/avatar', method: HttpMethod.Put, handler: this.uploadAvatar,
+      middlewares: [
+        new UploadFileMiddleware(this.config.get('UPLOAD_DIRECTORY'), 'avatar'),
+      ]
+    });
     this.addRoute({ path: '/favorites', method: HttpMethod.Get, handler: this.getFavorites });
     this.addRoute({
       path: '/favorites/:offerId', method: HttpMethod.Put, handler: this.putFavorites, middlewares: [
@@ -43,10 +51,14 @@ export class UserController extends BaseController {
     this.created(res, responseData);
   }
 
-  public async updateAvatar(req: Request, res: Response): Promise<void> {
+  public async uploadAvatar(req: Request, res: Response) {
     // 200	Изображение аватара успешно загружено.
     // 400	Неверный формат файла. Допускаются только изображения в формате PNG или JPG.
     // 401	Не авторизован. Пожалуйста, предоставьте валидный токен.
+
+    this.created(res, {
+      filepath: req.file?.path
+    });
   }
 
   public async getFavorites(req: Request, res: Response): Promise<void> {
