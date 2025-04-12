d424942c61b04968643f9d57fdba7a3bd6f1fd67
Yuriy <yura.posledov@yandex.ru>
2025-04-13 01:27:54 +0300

    9.3. Добавит ValidationExceptionFilter. WIP

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -2,6 +2,7 @@ export { RequestParams } from './types/request-params.type.js';
 export { RequestBody } from './types/request-body.type.js';
 export { HttpMethod } from './types/http-method.enum.js';
 export { ValidationErrorField } from './types/validation-error-field.type.js';
+export { ApplicationError } from './types/application-error.enum.js';
 export { Route } from './types/route.interface.js';
 export { Middleware } from './middleware/middleware.interface.js';
 export { ValidateObjectIdMiddleware } from './middleware/validate-objectid.middleware.js';
