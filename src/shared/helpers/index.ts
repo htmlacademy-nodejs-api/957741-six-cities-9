c0f250a54e6ae73fd8431029ad2edd3fecffacf6
Yuriy <yura.posledov@yandex.ru>
2025-04-13 01:20:32 +0300

    9.2. Добавит ошибку ValidationError

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -5,6 +5,7 @@ export {
   getErrorMessage,
   fillDTO,
   createErrorObject,
+  reduceValidationErrors,
 } from './common.js';
 export { getMongoURI } from './database.js';
 export { createSHA256 } from './hash.js';
--- src/shared/libs/rest/middleware/validate-dto.middleware.ts
+++ src/shared/libs/rest/middleware/validate-dto.middleware.ts
@@ -1,27 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
-import { StatusCodes } from 'http-status-codes';
 
 import { Middleware } from './middleware.interface.js';
-import { HttpError } from '../errors/index.js';
+import { reduceValidationErrors } from '../../../helpers/common.js';
+import { ValidationError } from '../errors/index.js';
 
 export class ValidateDtoMiddleware implements Middleware {
   constructor(private dto: ClassConstructor<object>) { }
 
-  public async execute({ body }: Request, res: Response, next: NextFunction): Promise<void> {
+  public async execute({ body, path }: Request, _res: Response, next: NextFunction): Promise<void> {
     const dtoInstance = plainToInstance(this.dto, body);
     const errors = await validate(dtoInstance);
 
     if (!errors.length) {
-      const messages = errors.map((error) =>
-        Object.values(error.constraints ?? {}).join(', ')
-      ).join('; ');
-      throw new HttpError(
-        StatusCodes.BAD_REQUEST,
-        `DTO Validation Error: ${messages}`,
-        'ValidateDtoMiddleware'
-      );
+      throw new ValidationError(`Validation error: ${path}`, reduceValidationErrors(errors));
     }
 
     next();
